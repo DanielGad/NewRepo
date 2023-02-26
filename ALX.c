4f8542a14f52abfaac6bdf107d657aeb982ee89b
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-02-26 23:59:20 +0100

    Add files via upload

---
+++ ALX.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int a;
+    int b;
+    char c;
+
+printf("Enter the first number: ");
+scanf("%lf", a);
+
+printf("Enter the second number: ");
+scanf("%lf", b);
+
+printf("What do you want to do;\nEnter 'sum' for addition\n'sub' for subtraction\n'quo' for Quotient\n'pro' for Product:\n ");
+scanf("%c", c);
+
+
+
+if (a + b)
+{
+    /* code */
+}
+
+
+return (0);
+}
